28fa3653b984e7b1317c723da5797d9425ba8ee8
Calum McConnell <calumlikesapplepie@gmail.com>
2026-01-15 23:04:51 -0500

    get distracted making a complete list of users instead

--- shiftsToCalendar.py
+++ shiftsToCalendar.py
@@ -9,29 +9,34 @@ with open("secrets.json") as fp:
 
 
 # selected "client secret" authentication flow, see source of code: https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=python#client-credentials-provider
-import azure.identity.aio as auth
 from  msgraph.graph_service_client import GraphServiceClient
 
-# The client credentials flow requires that you request the
-# /.default scope, and pre-configure your permissions on the
-# app registration in Azure. An administrator must grant consent
-# to those permissions beforehand.
-scopes = ['https://graph.microsoft.com/.default']
+graph_client: GraphServiceClient
+def initalize_auth():
+    import azure.identity.aio as auth
 
-# azure.identity.aio
-credential = auth.ClientSecretCredential(
-    tenant_id=secretsData["tenant_id"],
-    client_id=secretsData["client_id"],
-    client_secret=secretsData["client_secret"])
 
-graph_client = GraphServiceClient(credential, scopes)
+    # The client credentials flow requires that you request the
+    # /.default scope, and pre-configure your permissions on the
+    # app registration in Azure. An administrator must grant consent
+    # to those permissions beforehand.
+    scopes = ['https://graph.microsoft.com/.default']
+
+    # azure.identity.aio
+    credential = auth.ClientSecretCredential(
+        tenant_id=secretsData["tenant_id"],
+        client_id=secretsData["client_id"],
+        client_secret=secretsData["client_secret"])
+
+    graph_client = GraphServiceClient(credential, scopes)
 
 # based on snippet GraphExplorer
 
-from msgraph.generated.teams.item.schedule.shifts.shifts_request_builder import ShiftsRequestBuilder
-from kiota_abstractions.base_request_configuration import RequestConfiguration
 
 async def writeShiftsToJson():
+    from msgraph.generated.teams.item.schedule.shifts.shifts_request_builder import ShiftsRequestBuilder
+    from kiota_abstractions.base_request_configuration import RequestConfiguration
+
     request_configuration = RequestConfiguration(
         query_parameters = ShiftsRequestBuilder.ShiftsRequestBuilderGetQueryParameters(
             filter = "sharedShift/startDateTime ge 2026-01-15T00:00:00.000Z",
@@ -61,6 +66,35 @@ from collections import defaultdict
 import namer
 # dictionary of user names
 userIdToNameDict = defaultdict(lambda:namer.generate(seperator=" ", style="title"))
+
+"""
+Set up our dictionary of users; currently using hardcoded values
+"""
+def initializeUsers():
+    from kiota_serialization_json.json_parse_node_factory import JsonParseNodeFactory
+    from msgraph.generated.models.user_collection_response import UserCollectionResponse
+   
+    # two userData arrays
+    with open("testUserData1.json", "rb") as fp:
+        userData1 = fp.read()
+    with open("testUserData2.json", "rb") as fp:
+        userData2 = fp.read()
+    
+
+    rootNode = JsonParseNodeFactory().get_root_parse_node("application/json",userData1)
+    userCollection1 = rootNode.get_object_value(UserCollectionResponse)   
+    assert userCollection1.value is not None
+    rootNode = JsonParseNodeFactory().get_root_parse_node("application/json",userData2)
+    userCollection2 = rootNode.get_object_value(UserCollectionResponse)   
+    assert userCollection2.value is not None
+    userCollectionFull = userCollection1.value.copy() # we wont ever user userCol1 again, but still.
+    userCollectionFull.extend(userCollection2.value)
+
+    for user in userCollectionFull:
+        assert user.id is not None
+        if user.surname is None or user.given_name is None:
+            continue
+        userIdToNameDict[user.id] = f"{user.given_name} {user.surname}"
 
 
 import icalendar as ical
